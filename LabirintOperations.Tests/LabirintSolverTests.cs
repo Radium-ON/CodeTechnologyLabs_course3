5866ed97089e888d4cf04092c8e7f35fde025102
Rodion <Radium-ON@outlook.com>
2019-09-28 00:27:43 +0700

    Заглушки тестов для LabirintSolver

---
+++ LabirintOperations.Tests/LabirintSolverTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LabirintOperations.Tests
+{
+    [TestClass]
+    public class LabirintSolverTests
+    {
+        [TestMethod]
+        public void GetLabirintSolutionTest()
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
+        
+        [TestMethod]
+        public void InRangeTest()
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
+    }
+}
