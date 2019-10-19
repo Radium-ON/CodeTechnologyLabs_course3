2ea9aeffc465fc09e597fddd563c5517055f0831
Rodion <Radium-ON@outlook.com>
2019-10-19 23:06:23 +0700

    WPF project added.

---
+++ MazeAmazing_WPF/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+
+namespace MazeAmazing_WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ModernWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MazeAmazing_WPF/Views/Settings/About.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MazeAmazing_WPF.Views.Settings
+{
+    /// <summary>
+    /// Interaction logic for About.xaml
+    /// </summary>
+    public partial class About : UserControl
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+    }
+}
