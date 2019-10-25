55175b603623e1c6dcc6e0806757a92172939c3e
Rodion <Radium-ON@outlook.com>
2019-10-25 22:52:23 +0700

    Новый элемент интерфейса - отображает лабиринт в виде Grid.

---
+++ MazeAmazing_WPF/ViewModels/MazeControlViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MazeAmazing_WPF.ViewModels
+{
+	public class MazeControlViewModel : BindableBase
+	{
+        public MazeControlViewModel()
+        {
+
+        }
+	}
+}
---
+++ MazeAmazing_WPF/Views/UserControls/MazeControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using LabirintOperations;
+
+namespace MazeAmazing_WPF.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MazeControl
+    /// </summary>
+    public partial class MazeControl : UserControl
+    {
+        public MazeControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty MazeGridProperty = 
+            DependencyProperty.Register(
+                "MazeGrid", typeof(string), typeof(MazeControl), 
+                new FrameworkPropertyMetadata() { 
+                    PropertyChangedCallback = OnMazeGridChanged, 
+                    BindsTwoWayByDefault = true });
+ 
+        public MazeCell[,] MazeGrid
+        {
+            get { return (MazeCell[,])GetValue(MazeGridProperty ); }
+            set { this.SetValue(MazeGridProperty , value); }
+        }
+ 
+        private static void OnMazeGridChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.OldValue != e.NewValue)
+            {
+                // code to be executed on value update
+            }
+        }
+
+        private void UpdateGridInstance()
+        {
+
+        }
+    }
+}
