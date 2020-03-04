a0465bfde42da4935c821f4f696215a28714aeb4
Rodion <Radium-ON@outlook.com>
2020-03-04 11:14:27 +0700

    New openFileDialog service

---
+++ Services/IDialogService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IDialogService
+    {
+        string FilePath { get; set; }   // путь к выбранному файлу
+
+        void ShowMessage(string message);   // показ сообщения
+
+        bool OpenFileDialog();  // открытие файла
+        bool SaveFileDialog();  // сохранение файла
+    }
+}
